cedff3fa98c2ffb3d13dbf8e35d6f947504b931d
JohnPaul-Londono <johnplondono@gmail.com>
2022-08-23 14:28:29 -0500

    Map card body for products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Welcome to Fossil TCG</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
